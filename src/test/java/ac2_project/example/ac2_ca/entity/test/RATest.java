c7304109d78f5f5172df1923d352087b3aa1a043
Linguini <leobrazfonseca24@gmail.com>
2025-11-17 23:13:02 -0300

    ..

--- src/test/java/ac2_project/example/ac2_ca/entity/test/RATest.java
+++ src/test/java/ac2_project/example/ac2_ca/entity/test/RATest.java
@@ -37,7 +37,7 @@ class RATest {
         
         //dObject obj = new Object();
     	//assertFalse(ra1.equals(obj), "Um objeto RA não deve ser igual a um objeto de outra classe");
-    	assertFalse(ra1.equals(ra1), "Um objeto RA não deve ser igual a um objeto de outra classe");
+    	//assertFalse(ra1.equals(ra1), "Um objeto RA não deve ser igual a um objeto de outra classe");
         
         ra1.equals(ra2);
         assertFalse(ra1.equals(ra3), "RAs com números diferentes não devem ser iguais");
