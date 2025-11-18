7dd90774ce448870b314547ea689599e02cfda5c
Linguini <leobrazfonseca24@gmail.com>
2025-11-17 23:26:43 -0300

    Alterações Finais

--- src/main/java/ac2_project/example/ac2_ca/Ac2CaApplication.java
+++ src/main/java/ac2_project/example/ac2_ca/Ac2CaApplication.java
@@ -3,8 +3,6 @@ package ac2_project.example.ac2_ca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @SpringBootApplication
@@ -15,14 +13,6 @@ public class Ac2CaApplication {
 		SpringApplication.run(Ac2CaApplication.class, args);
 		
 	}
-		
-		@RequestMapping ("/")
-		@ResponseBody
-		String home()
-		{
-			return "Hello World";
-	}
-		
 		
 }
 
